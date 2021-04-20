6d9bd369bbb50eb22f6834d4634720c1da574eca
Connor Rayburn <connorrayburn.work@gmail.com>
2021-04-19 17:14:32 -0700

    completed MVP

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Route } from 'react-router-dom';
 
 import SavedList from './Movies/SavedList';
+import Movie from './Movies/Movie';
+import MovieList from './Movies/MovieList';
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -14,6 +17,8 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          setMovieList(response.data);
+          console.log(response.data);
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -30,7 +35,14 @@ export default function App () {
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
+      <Route exact path='/'>
+        <MovieList movies={movieList} />
+      </Route>
+      <Route path='/movies/:movieID'>
+          <Movie />
+          {/*this doesnt need to passed to Movie? movie={movieList} */}
+      </Route>
+
     </div>
   );
 }
